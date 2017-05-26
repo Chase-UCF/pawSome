5d8adbd082d063badf4898d2e0f3d79b4f6cd8d0
Chase-UCF <chaserobison8@gmail.com>
2017-05-25 20:47:16 -0400

    sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,11 @@
 CREATE DATABASE IF NOT EXISTS pets_db;
 USE pets_db;
 
-# Create the burgers table
-CREATE TABLE user (
-id int NOT NULL AUTO_INCREMENT,
-name varchar(255) NOT NULL,
-password varchar(255) NOT NULL,
-PRIMARY KEY (id)
-);
-
-CREATE TABLE answers (
-client_id int NOT NULL,
-answer1 varchar(255) NOT NULL,
-answer2 varchar(255) NOT NULL,
-answer3 varchar(255) NOT NULL,
-answer4 varchar(255) NOT NULL,
-answer5 varchar(255) NOT NULL,
-PRIMARY KEY (id)
+CREATE TABLE pets(
+type varchar(50) NOT NULL,
+name varchar(50) NOT NULL,
+description varchar(50) NOT NULL,
+age INT NOT NULL,
+sex BOOLEAN NOT NULL,
+img_url varchar(255)
 );
